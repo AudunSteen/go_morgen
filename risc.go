0b79245334875e4f2c1eec27714dc398749dc906
AudunSteen <audunste@uia.no>
2022-01-24 14:51:52 +0100

    rsc

--- risc.go
+++ risc.go
@@ -1,25 +1,14 @@
-package quote // import "rsc.io/quote"
+package main
 
-import "rsc.io/sampler"
+import (
+	"fmt"
+	"rsc.io/quote"
+)
 
-// Hello returns a greeting.
-func Hello() string {
-	return sampler.Hello()
-}
+func main() {
 
-// Glass returns a useful phrase for world travelers.
-func Glass() string {
-	// See http://www.oocities.org/nodotus/hbglass.html.
-	return "I can eat glass and it doesn't hurt me."
-}
-
-// Go returns a Go proverb.
-func Go() string {
-	return "Don't communicate by sharing memory, share memory by communicating."
-}
-
-// Opt returns an optimization truth.
-func Opt() string {
-	// Wisdom from ken.
-	return "If a program is too slow, it must have a loop."
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Opt())
 }
